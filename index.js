3668dbbd6e1b82125a3934caa0d4afdb98843631
nobirhoshensohag <nobirhoshensohag708@gmail.com>
2025-11-12 18:43:30 +0600

    all api challengescollection add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require("dotenv").config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -21,29 +21,244 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-     // All collection and Database
-    const db = client.db("assignment10");
+    // All collection and Database
+    const db = client.db("assigment10");
     const usersCollention = db.collection("users");
-    
+    const challengesCollection = db.collection("challenges");
+    const ecoTipsCollection = db.collection("eco_tips");
+    const eventsCollection = db.collection("events");
+    const subscribersCollection = db.collection("subscribers");
+    const challengesParticipantsCollection = db.collection("challengesParticipants");
+
+
 
     // ALl Methords 
-    app.post('/users', async (req, res)=>{
+    app.post('/users', async (req, res) => {
       const newUser = req.body;
       const email = req.body.email;
-      const query = {email: email}
+      const query = { email: email }
       const existingUser = await usersCollention.findOne(query);
-      if(existingUser){
-        res.send({message: 'User already exist, Do not need to insert again.'});
+      if (existingUser) {
+        res.send({ message: 'User already exist, Do not need to insert again.' });
       }
-      else{
+      else {
         const result = await usersCollention.insertOne(newUser);
         res.send(result);
 
       }
+    })
 
+    
+
+    // participants range filtering using $gte / $lte Example query:
+    app.get('/api/challenges', async (req, res) => {
+      try {
+        // Get query parameters from URL
+        const { participantsMin, participantsMax, category, startDateFrom, startDateTo } = req.query;
+
+        // Create dynamic filter object
+        const filter = {};
+
+        // participants range filtering using $gte / $lte  (/api/challenges?participantsMin=10&participantsMax=100)
+        if (participantsMin || participantsMax) {
+          filter.participants = {};
+          if (participantsMin) filter.participants.$gte = parseInt(participantsMin);
+          if (participantsMax) filter.participants.$lte = parseInt(participantsMax);
+        }
+
+
+
+
+        
+        if (category) {
+          // Split comma-separated categories into array
+          const categories = Array.isArray(category) ? category : category.split(',');
+          filter.category = { $in: categories };
+        }
+       
+
+
+
+    // StartDate range filter
+    if (startDateFrom || startDateTo) {
+      filter.startDate = {};
+      if (startDateFrom) filter.startDate.$gte = startDateFrom;
+      if (startDateTo) filter.startDate.$lte = startDateTo;
+    }
+    
+
+
+
+
+    
+        // Fetch filtered data from MongoDB
+        const result = await challengesCollection.find(filter).toArray();
+
+
+
+        // Send response
+        res.send(result);
+      } catch (error) {
+        console.error('Error fetching challenges:', error);
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+
+      
+    });
+
+
+
+
+
+    // find  challengesCollection all data
+    app.get('/api/participants', async (req, res) => {
+      const cursor = challengesParticipantsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+
+
+
+
+
+    // Add challenges Method    
+    app.post('/api/challenges', async (req, res) => {
+      const newChallenge = req.body;
+      const { createdBy, title } = newChallenge;
+
+      // Check if the same user already added this specific challenge title
+      const query = { createdBy: createdBy, title: title };
+      const existingChallenge = await challengesCollection.findOne(query);
+
+      if (existingChallenge) {
+        return res.status(400).send({
+          success: false,
+          message: 'You have already added this challenge. Please choose a different title.'
+        });
+      }
+
+      // If not found, insert the new challenge
+      const result = await challengesCollection.insertOne(newChallenge);
+      res.send({ success: true, result });
+    });
+
+
+
+
+    // POST /api/participants - Add new participant and increase challenge count
+    app.post('/api/participants', async (req, res) => {
+      try {
+        const participant = req.body;
+        const { participantEmail, challengeId } = participant;
+
+        // Check if the participant already joined this challenge
+        const existing = await challengesParticipantsCollection.findOne({
+          participantEmail,
+          challengeId
+        });
+
+        if (existing) {
+          return res.status(400).send({
+            message: 'You have already joined this challenge!',
+          });
+        }
+
+        // Insert the new participant
+        const result = await challengesParticipantsCollection.insertOne(participant);
+
+        //  Update participants count in related challenge
+        await challengesCollection.updateOne(
+          { _id: new ObjectId(challengeId) },
+          { $inc: { participants: 1 } }
+        );
+
+        res.status(201).send({
+          message: 'Participant joined successfully',
+          result,
+        });
+      } catch (error) {
+        console.error('Error adding participant:', error);
+        res.status(500).send({ message: 'Server error', error });
+      }
+    });
+
+
+    // New subscriber Api 
+    app.post("/api/subscribe", async (req, res) => {
+      const { name, email } = req.body;
+
+      if (!name || !email) {
+        return res.status(400).json({ message: "Name and Email are required" });
+      }
+
+      try {
+        // Check if email already exists
+        const existing = await subscribersCollection.findOne({ email });
+        if (existing) {
+          return res.status(400).json({ message: "Email already subscribed" });
+        }
+
+        // Insert new subscriber
+        const result = await subscribersCollection.insertOne({
+          name,
+          email,
+          createdAt: new Date(),
+        });
+
+        res.status(201).json({ message: "Subscribed successfully!", subscriberId: result.insertedId });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Server error" });
+      }
+    });
+
+
+
+
+
+    // EcoTips Collection 
+    app.get('/api/eco-tips', async (req, res) => {
+      const cursor = ecoTipsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+      // console.log(result);
+    })
+
+    // PATCH /api/eco-tips/:id/upvote for update recent Tips
+    app.patch('/api/ecotips/:id/upvote', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const update = { $inc: { upvotes: 1 }, $set: { updatedAt: new Date() } };
+        const result = await ecoTipsCollection.updateOne(query, update);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Failed to update upvotes" });
+      }
+    });
+
+    // 
+
+    // events Collection 
+    app.get('/api/events', async (req, res) => {
+      const cursor = eventsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+      // console.log(result);
+    })
+
+    // Find single event
+    app.get('/api/events/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await eventsCollection.findOne(query);
+      res.send(result);
     })
 
     
